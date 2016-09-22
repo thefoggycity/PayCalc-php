d4f1ec177cf9ed9a5a068377f15a4a92864be619
unknown <>
2016-09-22 18:45:52 +0800

    Implemented data calc

--- Mod_Calc.php
+++ Mod_Calc.php
@@ -1,8 +1,8 @@
 <?php
 
 class pHeader{
-	var $people;
-	var $nameList;
+	var $people;    //Number of people involved
+	var $nameList;  //People's namelist (in order)
 	function __construct(array $names){
 		$this->people = count($names);
 		$this->nameList = $names;
@@ -12,7 +12,7 @@ class pHeader{
 	}
 }
 
-class transaction{
+class trans{
     var $item;		//Name of transaction
     var $people;	//Number of people involved (should be globally identical)
     var $paid;		//Array indicates each person's payment
@@ -31,7 +31,6 @@ class transaction{
 		 * Normally returns 0, returns -n while nth parameter goes wrong.
 		 */
         if (count($payment) == $this->people){
-            var_dump($payment);
             $this->paid = $payment;
         }
         else return -1;
@@ -57,14 +56,89 @@ class transaction{
 }
 
 class transLog{
-	var $head;
-	var $transNum;
-	var $transList;
-	var $paySum;
-	var $dueSum;
+	var $head;      //People's details as a log header (pHeader)
+	var $transNum;  //Number of transactions
+	var $transList; //Array of transactions (contain object trans)
+	var $paySum;    //Array of every persons' total payment
+	var $dueSum;    //Array of every persons' total expenses
+    var $netSum;    //Array of every persons' net bill (- as in debt)
+
+    function __construct(pHeader $people_log_header){
+        $this->head = $people_log_header;
+        $this->transNum = 0;
+        $this->transList = array();
+        $this->paySum = array_fill(0, $this->head->people, 0.0);
+        $this->dueSum = array_fill(0, $this->head->people, 0.0);
+        $this->netSum = array_fill(0, $this->head->people, 0.0);
+    }
+    function setTransList(array $transaction_list){
+        $this->transNum = count($transaction_list);
+        $this->transList = $transaction_list;
+        $this->updateSums();
+    }
+    function addTransLog(trans $transaction){
+        $this->transNum++;
+        $this->transList[] = $transaction;
+        for($i = 0; $i < $this->head->people; $i++){
+            $this->paySum[$i] += $transaction->paid[$i];
+            $this->dueSum[$i] += $transaction->consume[$i];
+            $this->netSum[$i] = $this->paySum[$i] - $this->dueSum[$i];
+        }
+    }
+    function updateSums(){
+        if ($this->transNum == 0) return -1;
+        $this->paySum = array_fill(0, $this->head->people, 0.0);
+        $this->dueSum = array_fill(0, $this->head->people, 0.0);
+        foreach($this->transList as $tr){
+            for($i = 0; $i < $this->head->people; $i++){
+                $this->paySum[$i] += $tr->paid[$i];
+                $this->dueSum[$i] += $tr->consume[$i];
+            }
+        }
+        for($i = 0; $i < $this->head->people; $i++){
+            $this->netSum[$i] = $this->paySum[$i] - $this->dueSum[$i];
+        }
+    }
+    function getSumsByIndex(int $index, bool $net_only = false){
+        if ($net_only){
+            return $this->netSum[$index];
+        }
+        else {
+            return array($this->paySum[$index], $this->dueSum[$index], 
+                $this->netSum[$index]);
+        }
+    }
+    function getSumsByName(string $name, bool $net_only = false){
+        //PLEASE AVOID USING SAME NAMES!! (array_search may return the first one)
+        $id = array_search($name, $this->head->nameList);
+        if ($id >= 0 && $id < $this->head->people)
+            return $this->getSumsByIndex($id, $net_only);
+    }
 }
 
-$h = new pHeader(array('a','b','c'));
-echo $h->findIndex('a');
+//Test code
+
+$h = new pHeader(array('a', 'b', 'c'));
+$l = new transLog($h);
+
+$t1 = new trans('t1', $h->people);
+$t1->record(array(10, 0, 80));
+$t2 = new trans('t2', $h->people);
+$t2->record(array(50, 0, 20), array(1, 0, 2));
+
+$tg = array($t1, $t2);
+$l->setTransList($tg);
+
+var_dump($l->getSumsByIndex(1));
+var_dump($l->getSumsByIndex(2, true));
+var_dump($l->getSumsByName('a'));
+
+$t3 = new trans('t3', $h->people);
+$t3->record(array(30,30,40), 'even');
+$l->addTransLog($t3);
+
+var_dump($l->getSumsByIndex(1));
+var_dump($l->getSumsByIndex(2, false));
+var_dump($l->getSumsByName('a'));
 
 ?>
